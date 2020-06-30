dab32343f4cdbee06765d4017719e3756b31a378
mfjimenezmoreno <42391444+mfjimenezmoreno@users.noreply.github.com>
2020-06-29 20:50:31 -0500

    Generation of second set of data

--- Python app/Convert_CV_to_Raw.py
+++ Python app/Convert_CV_to_Raw.py
@@ -1,8 +1,15 @@
+
 import pandas as pd
-from eLoaD_functions import current_to_raw
+from eLoaD_functions import current_to_raw, raw_to_Current
 
 CV = pd.read_csv(r'Cap.csv')
 CV.columns = ['Voltage', 'Current']
 CV['Raw']=CV['Current'].apply(lambda x:current_to_raw(x,muxGain=300000)[0])
 CV['ADC_Volts']=CV['Current'].apply(lambda x:current_to_raw(x,muxGain=300000)[1])
-CV.to_csv("Cap_raw.csv", columns=['Voltage','Raw'])
+CV.to_csv("Cap_raw.csv", columns=['Voltage','Raw'], index=0)
+
+#Let's now try if our libraries werk
+CV2 = pd.read_csv(r'Cap_raw.csv')
+CV2['Current'] = CV2['Raw'].apply(lambda x: raw_to_Current(x, muxGain=300000)[0])
+CV2.to_csv("Cap_raw_current2.csv", columns=['Voltage', 'Current'], index=0)
+
