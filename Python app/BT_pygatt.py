ffb7cbae78b8989e9c499358cce33a17ff447db2
mfjimenezmoreno <42391444+mfjimenezmoreno@users.noreply.github.com>
2020-07-16 23:06:59 -0500

    BT update

--- Python app/BT_pygatt.py
+++ Python app/BT_pygatt.py
@@ -1,17 +1,25 @@
+from binascii import hexlify
 import pygatt
+import logging
+
 
 MACADDRESS = "7c:ec:79:69:62:f8"
-SERVICE = "0000ffe0-0000-1000-8000-00805f9b34fb"
+SERVICE = "0000ffe1-0000-1000-8000-00805f9b34fb"
 adapter = pygatt.BGAPIBackend(serial_port="COM15")
 
-try:
-    adapter.start()
-    device = adapter.connect(MACADDRESS)
-    characteristics = device.discover_characteristics()
-    for key in characteristics.keys():
-        print(str(key) + ": " + str(characteristics[key]))
-    SERVICE = "0000ffe1-0000-1000-8000-00805f9b34fb"
-    device.char_write(value=[1],uuid=SERVICE)
-    value = device.char_read(SERVICE)
-finally:
-    adapter.stop()
+
+def handle_data(handle, value):
+    """
+    handle -- integer, characteristic read handle the data was received on
+    value -- bytearray, the data returned in the notification
+    """
+    print("Received data: %s" % hexlify(value))
+
+adapter.start()
+device = adapter.connect(MACADDRESS)
+characteristics = device.discover_characteristics()
+device.subscribe(SERVICE, callback=handle_data)
+
+device.char_write(hexlify("01"),uuid=SERVICE)
+value = device.char_read(SERVICE)
+adapter.stop()
